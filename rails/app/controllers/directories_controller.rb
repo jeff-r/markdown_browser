83e0e11672eff6c9dec01a0c7d619cd6be13ae10
Jeff Roush <jeff@jeffroush.com>
2018-10-04 10:37:09 -0700

    Add rubocop and solargraph

--- rails/app/controllers/directories_controller.rb
+++ rails/app/controllers/directories_controller.rb
@@ -1,38 +1,47 @@
+# frozen_string_literal: true
+
 class DirectoriesController < ApplicationController
   def index
-    render json: {filenames: files}
+    render json: { filenames: files }
   end
 
   def jobs_dir
-    "/home/jeff/Dropbox/docs/jeffs-job-folder/2018"
+    '/home/jeff/Dropbox/docs/jeffs-job-folder/2018'
   end
 
   def test_dir
-    "/home/jeff/Dropbox/projects/javascript/react/notes/notes_rails/tmp/markdown"
+    '/home/jeff/Dropbox/projects/javascript/react/notes/notes_rails/tmp/markdown'
   end
-
 
   def files(dir = jobs_dir)
     paths = Dir.glob("#{dir}/*")
     paths.map do |path|
       if File.directory?(path)
-        {
-          directoryName: File.basename(path),
-          files: files(path)
-        }
+        dir_hash path
       else
-        {
-          fileName: File.basename(path),
-          content: file_contents(path),
-          type: File.ftype(path)
-        }
+        file_hash path
       end
     end
   end
 
+  def file_hash(path)
+    {
+      fileName: File.basename(path),
+      content: file_contents(path),
+      type: File.ftype(path)
+    }
+  end
+
+  def dir_hash(path)
+    {
+      directoryName: File.basename(path),
+      files: files(path)
+    }
+  end
+
   def renderable?(filename)
     extension = File.extname(filename)
-    extension == ".md" || extension == ".txt"
+    ['.md', '.txt'].include? extension
   end
 
   def file_contents(filename)
