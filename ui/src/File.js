9fd529483f1fe57116db05238f4e24577925a089
Jeff Roush <jeff@jeffroush.com>
2018-10-07 14:15:44 -0700

    Move ReactRouterLinkRenderer into its own file

--- ui/src/File.js
+++ ui/src/File.js
@@ -1,18 +1,7 @@
 import React from "react";
 import { renderNodes } from "simple-commonmark-react";
 
-import { createElement } from "react";
-import { Link } from "react-router-dom";
-import LinkRenderer from "simple-commonmark-react/dist/renderers/LinkRenderer";
-
-export default class ReactRouterLinkRenderer extends LinkRenderer {
-  renderNodeWithProps(props) {
-    const url = props.href;
-    delete props.href;
-    props.to = url;
-    return createElement(Link, props, []);
-  }
-}
+import ReactRouterLinkRenderer from "./ReactRouterLinkRenderer";
 
 export const File = ({ file, onEditClicked }) => {
   const { fileName, content } = file;
