7197939705f3050d27dc91b284168d76bc446478
Jeff Roush <jeff@jeffroush.com>
2018-10-11 16:47:58 -0700

    Add a toggle to show or hide the file list

--- ui_redux/src/components/FileList.jsx
+++ ui_redux/src/components/FileList.jsx
@@ -3,6 +3,14 @@ import { FileListItem } from "./FileListItem";
 import { fetchPath } from "../api/fetchPath";
 
 export class FileList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showFileList: true
+    };
+  }
+
   componentWillUpdate(nextProps) {
     this.updateContentIfNeeded(nextProps.currentFilename);
   }
@@ -39,20 +47,40 @@ export class FileList extends React.Component {
     return this.props.files.sort((a, b) => a.filename > b.filename);
   };
 
+  onListDisplayLinkClicked = () => {
+    this.setState({ showFileList: !this.state.showFileList });
+  };
+
   render() {
     let { fetchFileContent, addFileContent } = this.props;
 
+    let displayLinkClass = this.state.showFileList
+      ? "show-file-list"
+      : "hide-file-list";
+
+    let displayLinkText = this.state.showFileList ? "<<" : ">>";
+
     return (
       <div className="file-list">
-        {this.topFiles().map(file => (
-          <FileListItem
-            key={file.filename}
-            file={file}
-            fetchFileContent={fetchFileContent}
-            addFileContent={addFileContent}
-            dispatch={this.props.dispatch}
-          />
-        ))}
+        <div>
+          <a
+            className="toggle-visibility-link"
+            onClick={this.onListDisplayLinkClicked}
+          >
+            {displayLinkText}
+          </a>
+        </div>
+        <div className={displayLinkClass}>
+          {this.topFiles().map(file => (
+            <FileListItem
+              key={file.filename}
+              file={file}
+              fetchFileContent={fetchFileContent}
+              addFileContent={addFileContent}
+              dispatch={this.props.dispatch}
+            />
+          ))}
+        </div>
       </div>
     );
   }
